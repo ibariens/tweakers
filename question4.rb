fd2525fcf32e32669cf0a55bed478765c6082f94
Bart Ariens <bart.ariens@divimove.com>
2015-09-02 15:41:11 +0200

    gives the right answer to question 4

--- question4.rb
+++ question4.rb
@@ -1,10 +1,14 @@
 @amount_of_days = 1000
 
-def fab(index, first_number, sec_number)
+def fab(index, total, first_number, sec_number)
+  total = total + first_number
+  # puts "day: #{index} diameter grows with #{first_number} resulting in total diameter of #{total}"
+
   if index == @amount_of_days
-    return first_number
+    return total
   end
-  fab(index +1, sec_number, first_number + sec_number)
+  
+  fab(index +1, total, sec_number, first_number + sec_number)
 end
 	
-puts (fab(0, 0, 1) * Math::PI)
+puts (fab(1, 0, 0, 1) * Math::PI) # 2.2095E+209
