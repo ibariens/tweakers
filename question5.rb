7829119aa3e8e598b28db52f3fa30784a0ed0743
bart.ariens@divimove.com <Stamp0t1984>
2015-09-11 18:33:33 +0200

    adds question 5 correctly

--- question5.rb
+++ question5.rb
@@ -2,19 +2,19 @@
 
 class Fixnum
   ROMAN_NUMBERS = {
-    1000 => "M",  
-     900 => "CM",  
-     500 => "D",  
+    1000 => "M",
+     900 => "CM",
+     500 => "D",
      400 => "CD",
-     100 => "C",  
-      90 => "XC",  
-      50 => "L",  
-      40 => "XL",  
-      10 => "X",  
-        9 => "IX",  
-        5 => "V",  
-        4 => "IV",  
-        1 => "I",  
+     100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
   }
 
   def roman
@@ -28,5 +28,25 @@ class Fixnum
   end
 end
 
+amount_of_stones = 12500000
+year = 0
 
-puts 5.roman
+def stones_needed(year)
+  year.roman.length * 250
+end
+
+def earth_quake_factor(year)
+  if year%43 == 0
+    return 1-0.15
+  else
+    return 1
+  end
+end
+
+while amount_of_stones > 0
+  year = year + 1
+  amount_of_stones = amount_of_stones*earth_quake_factor(year)
+  amount_of_stones = amount_of_stones - stones_needed(year)
+
+  puts "in year: #{year} (#{year.roman}) we have #{amount_of_stones} left"
+end
