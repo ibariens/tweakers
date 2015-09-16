341df86593186ad2f142ee6620f0258a0a308f22
Bart Ariens <bart.ariens@divimove.com>
2015-09-16 09:37:16 +0200

    adds a progress-bar since it's very time consuming to calculate 2^199 combinations

--- question10.rb
+++ question10.rb
@@ -1,9 +1,26 @@
+require 'ruby-progressbar'
+
 @lines = File.open('vraag_10_triangle.txt').readlines
 @max_amount = 0
 
+
+till_number = 100000000
+amount_omkeerbaar = 0
+@progress_bar = ProgressBar.create( :format         => '%a %bᗧ%i %p%% %t',
+                                    :progress_mark  => ' ',
+                                    :remainder_mark => '･',
+                                    :starting_at    => 0,
+		                    :total => 2**(@lines.length-1))
+
+
+
 def find_max(line_index, number_position, path)
 
-  if line_index < @lines.length()
+  if line_index == @lines.length()
+    sum_path    = path.inject(:+)
+    @max_amount = sum_path if sum_path > @max_amount
+    @progress_bar.increment
+  else
     positions = [number_position, number_position+1]
     new_line_index = line_index + 1
 
@@ -16,9 +33,6 @@ def find_max(line_index, number_position, path)
         find_max(new_line_index, pos, copy)
       end
     end
-  else
-    sum_path    = path.inject(:+)
-    @max_amount = sum_path if sum_path > @max_amount
   end
 end
 
